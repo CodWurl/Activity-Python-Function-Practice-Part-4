fd75423fda35832a0ef5e4e6b4fd8ad3799ba4fd
CodWurl <46231725+CodWurl@users.noreply.github.com>
2022-10-29 13:49:57 -0400

    added pascal for triangle

--- main.py
+++ main.py
@@ -37,32 +37,31 @@ print(mult_list([]))
 def rev_string(my_str):
   return my_str[::-1]
 
-print(rev_string(""))
-print(rev_string("apple"))
-print(rev_string("mt string"))
+print(rev_string("This should be reversed"))
+
 
 # Write a Python function called num_within() to check whether a number falls in a given range.
 
 def num_within(x,a,b):
   return x in range(a,b+1)
      
-print(num_within(3,2,4))     
-print(num_within(3,1,3))     
-print(num_within(10,2,5))
+print(num_within(5,4,20))     
+print(num_within(909998098,4,20))     
+
 
 # Write a Python function called pascal() that prints out the first n rows of Pascal's triangle.
 
 triangle = [[1],[1,1]]
-def pascal(n):
+def pascal(depth):
   #base case
-  if n < 1:
+  if depth < 1:
     print("invalid number of rows")
-  elif n == 1:
-    print(triangle[0])
+  elif depth == 1:
+    print([0])
   else:
     row_number = 2
     #fill up correct number of rows in triangle
-    while len(triangle) < n:
+    while len(triangle) < depth:
       row = []
       row_prev = triangle[row_number - 1]
       #create correct row, then add to triangle (this row will be 1 longer than row before it)
